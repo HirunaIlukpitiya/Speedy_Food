0e9489da79679bfa6790d8d0c048b01ef0d3f0ed
HirunaIlukpitiya <33183435+HirunaIlukpitiya@users.noreply.github.com>
2023-07-03 21:36:56 +0530

    backend delete

--- backend/config/keys.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    mongoURI: "mongodb+srv://hiruna123:rGlEZ1qS7ltYOZaF@cluster0.v3j0ubs.mongodb.net/test"
-  };
-
-  module.exports = {
-    mongoURI: "mongodb+srv://hiruna123:rGlEZ1qS7ltYOZaF@cluster0.v3j0ubs.mongodb.net/test" ,
-    secretOrKey: "secret"
-  };
